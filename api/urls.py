dc5733bbb4225dddd3799f58a61898d739910120
Arjun Dixit <arjun.dixit98@gmail.com>
2025-05-04 23:26:24 +0530

    undo last change

--- api/urls.py
+++ api/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     #wishlist urls
     path('wishlist', wishlist_views.WishListAPIView.as_view(), name="get-wishlist"),
     path('wishlist/<int:wishlistid>', wishlist_views.WishListAPIView.as_view(), name="delete-wishlist"),
-    path('wishlists/', wishlist_views.AllWishListsAPIView.as_view(), name="get-wishlists"),
+    path('wishlists', wishlist_views.AllWishListsAPIView.as_view(), name="get-wishlists"),
 
     #zerodha holdings urls
     path('holding', holdings_views.StockHoldingAPIView.as_view(), name="get-holding"),
